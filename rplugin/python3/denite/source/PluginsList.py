132a17912709189f47c66380d65ea80011a1f20a
mirachan010 <mirachan010@gmail.com>
2020-09-22 17:05:26 +0900

    これじゃだめ

--- rplugin/python3/denite/source/PluginsList.py
+++ rplugin/python3/denite/source/PluginsList.py
@@ -11,7 +11,11 @@ class Source(Base):
     def gather_candidates(self, context):
         cmd = ['cat', '~/vim-pluginlist/list']
         candidates = []
-        for path in subprocess.run(cmd, check=True, universal_newlines=True, stdout=subprocess.PIPE).stdout.split()
-            print(aa)
-            candidates += [{'word': "{0} {1}".format(path, "test"),'action__text': path}]
-        return candidates
+        return [{'word': path, 'action__path': path}
+        for path in subprocess.run(cmd,
+                check=True,
+                universal_newlines=True,
+                stdout=subprocess.PIPE
+                ).stdout.split()]
+            # candidates += [{'word': "{0} {1}".format(path, "test"),'action__text': path}]
+        # return candidates
