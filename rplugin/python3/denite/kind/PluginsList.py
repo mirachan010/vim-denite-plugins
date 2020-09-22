2b922fd2d9bf21078bf0a555cb4d8a5cbb47a694
mirachan010 <mirachan010@gmail.com>
2020-09-22 18:23:59 +0900

    make action

--- rplugin/python3/denite/kind/PluginsList.py
+++ rplugin/python3/denite/kind/PluginsList.py
@@ -10,26 +10,26 @@ class Kind(Base):
         # action_xxx() が呼び出される
         self.default_action = 'pluginslist'
 
-        def action_pluginslist(self, context):
-            '''
-            コンストラクタで指定したように，デフォルトのアクションを記述する関数
-            '''
-            for target in context['targets']:
-                filepath = target['word']
-                filepath = "~/vim-denite-plugins/ReadMe" & str(filepath)
-                print(filepath + ' has ' + str(sum(1 for line in open(filepath, encoding='utf-8'))) + ' lines')
-
-        def action_preview(self, context):
-            '''
-            候補上にカーソルが移動する度に呼び出される関数
-            :Denite -auto-preview ... のように -auto-preview オプションを付加すると，previewモードになる
-            付加しない場合でも，以下のキーを押下することでpreview動作を行うことは可能
-            insertモード: <C-v>
-            normalモード: p
-            return Trueとしないと，deniteが終了する
-            '''
-            # context['targets'] に source側のgether_candidatesの返り値が格納されている
-            filepath = context['targets'][0]['word']
+    def action_pluginslist(self, context):
+        '''
+        コンストラクタで指定したように，デフォルトのアクションを記述する関数
+        '''
+        for target in context['targets']:
+            filepath = target['word']
             filepath = "~/vim-denite-plugins/ReadMe" & str(filepath)
             print(filepath + ' has ' + str(sum(1 for line in open(filepath, encoding='utf-8'))) + ' lines')
-            return True
+
+    def action_preview(self, context):
+        '''
+        候補上にカーソルが移動する度に呼び出される関数
+        :Denite -auto-preview ... のように -auto-preview オプションを付加すると，previewモードになる
+        付加しない場合でも，以下のキーを押下することでpreview動作を行うことは可能
+        insertモード: <C-v>
+        normalモード: p
+        return Trueとしないと，deniteが終了する
+        '''
+        # context['targets'] に source側のgether_candidatesの返り値が格納されている
+        filepath = context['targets'][0]['word']
+        filepath = "~/vim-denite-plugins/ReadMe" & str(filepath)
+        print(filepath + ' has ' + str(sum(1 for line in open(filepath, encoding='utf-8'))) + ' lines')
+        return True
