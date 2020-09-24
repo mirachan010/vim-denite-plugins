271d5b153501dd4d4f7a2f6b894923ab6766b2f4
mirachan010 <mirachan010@gmail.com>
2020-09-24 16:05:53 +0900

    make

--- rplugin/python3/denite/kind/PluginsList.py
+++ rplugin/python3/denite/kind/PluginsList.py
@@ -13,7 +13,7 @@ class Kind(Base):
             self.vim.command('e ' + str(filepath))
 
     def action_preview(self, context):
-        filepath = target['word']
+        filepath = context['word']
         filepath = "~/vim-pluginlist/ReadMe/" + str(filepath)
         self.vim.command('ped ' + str(filepath))
         return True
