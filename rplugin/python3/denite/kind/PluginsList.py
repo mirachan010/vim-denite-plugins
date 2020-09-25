43b0eff2831b0a243a402adda3271916c5184eea
mirachan010 <mirachan010@gmail.com>
2020-09-25 22:08:43 +0900

    aaaa

--- rplugin/python3/denite/kind/PluginsList.py
+++ rplugin/python3/denite/kind/PluginsList.py
@@ -17,7 +17,7 @@ class Kind(Base):
         for target in context['targets']:
             filepath = target['word']
             filepath = "~/vim-pluginlist/ReadMe/" + str(filepath)
+            self.vim.command('pclose!')
             self.vim.command('vs')
             self.vim.command('setl previewwindow')
             self.vim.command('e ' + str(filepath))
-            self.vim.command('pclose!')
