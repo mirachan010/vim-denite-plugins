746870f5d63ea329669d1df41be15d700a094b9c
mirachan010 <mirachan010@gmail.com>
2020-09-28 00:34:35 +0900

    このリポジトリ内にぶっこんでる者のlistを見るように変更

--- rplugin/python3/denite/source/PluginsList.py
+++ rplugin/python3/denite/source/PluginsList.py
@@ -11,7 +11,6 @@ class Source(Base):
 
     def gather_candidates(self, context):
         FilePath = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../../../vim-pluginlist/list"))
-        # cmd = ['cat', '~/vim-pluginlist/list']
         cmd = ['cat', FilePath]
         candidates = []
         return [{'word': path}
