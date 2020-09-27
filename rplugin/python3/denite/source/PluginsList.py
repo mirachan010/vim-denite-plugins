cc42d950a62f79f73c1463acbbeb483ff60afb9e
mirachan010 <mirachan010@gmail.com>
2020-09-28 00:40:23 +0900

    aa

--- rplugin/python3/denite/source/PluginsList.py
+++ rplugin/python3/denite/source/PluginsList.py
@@ -10,7 +10,7 @@ class Source(Base):
         self.kind = 'pluginslist'
 
     def gather_candidates(self, context):
-        FilePath = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../../../vim-pluginlist/list"))
+        FilePath = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../../../../vim-pluginlist/list"))
         cmd = ['cat', FilePath]
         candidates = []
         return [{'word': path}
