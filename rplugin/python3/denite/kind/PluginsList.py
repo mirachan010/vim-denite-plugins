4711137621fdbbe170b13c15d1ef08563415f15d
mirachan010 <mirachan010@gmail.com>
2020-10-02 19:33:10 +0900

    ghをいれてても使わないことができるようになった。これで選択できるようになる

--- rplugin/python3/denite/kind/PluginsList.py
+++ rplugin/python3/denite/kind/PluginsList.py
@@ -22,23 +22,16 @@ class Kind(Base):
             self.vim.command('s /^/[[plugins]]\rrepo = \'')
             self.vim.command('nohlsearch')
 
-    # def action_preview(self, context):
-    #     for target in context['targets']:
-    #         filepath = target['word']
-    #         filepath = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../../../../vim-pluginlist/ReadMe/" + str(filepath)))
-    #         self.vim.command('pclose!')
-    #         self.vim.command('vs')
-    #         self.vim.command('setl previewwindow')
-    #         self.vim.command('e ' + str(filepath))
-
     # def action_PluginsReadme(self, context):
     def action_preview(self, context):
         self.vim.command('let check_ghvim_for_readme = !empty(globpath(&rtp, "autoload/gh/gh.vim"))')
-        check = self.vim.eval("check_ghvim_for_readme")
+        ghcheck = self.vim.eval("check_ghvim_for_readme")
+        self.vim.command('let use_gh = get(g:, "pluginlist_use_gh", 0)')
+        ghuse = self.vim.eval("use_gh")
         for target in context['targets']:
             filepath = target['word']
             self.vim.command('pclose!')
-            if check:
+            if ghcheck and ghuse:
                     self.vim.command('new gh://' + str(filepath) + '/readme')
             else:
                     filepath = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../../../../vim-pluginlist/ReadMe/" + str(filepath)))
